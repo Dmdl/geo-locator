097dd5c0e515ea65202505715293d1726fec6467
Lakmal Dasanayake <lakmal@canva.com>
2023-09-14 20:22:49 +1000

    sql

--- src/main/resources/static/db.sql
+++ src/main/resources/static/db.sql
@@ -4,7 +4,7 @@ create table speedometer_reading
 (
     _id        INT             NOT NULL AUTO_INCREMENT,
     coordinate POINT           NOT NULL,
-    reading    DECIMAL(6, 2)   NOT NULL,
+    reading    DECIMAL(6, 2),
     x_value    DECIMAL(15, 10) NOT NULL,
     y_value    DECIMAL(15, 10) NOT NULL,
     z_value    DECIMAL(15, 10) NOT NULL,
